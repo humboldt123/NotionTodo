0c87c9b539bab1c5368e32dc65dd0ebd556ce7e8
Vish <38266782+humboldt123@users.noreply.github.com>
2024-08-26 22:14:00 -0400

    Initial Commit

---
+++ NotionTodo/NotionTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotionTodoApp.swift
+//  NotionTodo
+//
+//  Created by Vish on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct NotionTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
